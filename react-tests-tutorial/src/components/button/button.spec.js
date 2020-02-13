812f6fe0799132245e867c193443216fd6925da5
Erik Parso <parsoerik@gmail.com>
2020-02-13 04:59:21 -0800

    Should emit callback on click event test.

--- react-tests-tutorial/src/components/button/button.spec.js
+++ react-tests-tutorial/src/components/button/button.spec.js
@@ -8,14 +8,25 @@ describe('DaherdButton Component', () => {
     describe('Renders', () => {
 
         let component;
+        let mockFunc;
 
         beforeEach(() => {
-            component = shallow(<SharedButton buttonText="Test button" emitevent={() => {}}/>);
+            mockFunc = jest.fn();
+            component = shallow(<SharedButton
+                buttonText="Test button"
+                emitEvent={mockFunc} />);
         });
 
         it("Shoult render a button", () => {
             const btn = findByDataTestAttr(component, "buttonComponent");
             expect(btn.length).toBe(1);
+        });
+
+        it("Should emit callback on click event", () => {
+            const btn = findByDataTestAttr(component, "buttonComponent");
+            btn.simulate("click");
+            const callback = mockFunc.mock.calls.length;
+            expect(callback).toBe(1);
         });
 
     });
